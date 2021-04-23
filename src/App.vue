c3459cb0241f9a2aceb576056d9f15e07697a859
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2021-04-23 17:03:00 +0800

    reconnected bootstrap

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,4 @@ export default {
 };
 </script>
 
-<style>
-  @import url(https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css);
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './App.vue';
-
-import store from './store';
+import 'bootstrap/dist/css/bootstrap.css';
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex);
+
+import store from './store';
 
 new Vue({
   render: h => h(App),
